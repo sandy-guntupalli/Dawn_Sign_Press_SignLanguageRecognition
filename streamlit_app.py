b27375ce688e011852518c4f9568d582863be56e
Sandy Guntupalli <72150665+sandy-guntupalli@users.noreply.github.com>
2024-08-25 17:52:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
 def main():
     st.sidebar.title("ASL AI " + ":i_love_you_hand_sign:")
@@ -11,4 +7,7 @@ def main():
 
     # Create custom navigation buttons for both "About" and "Model"
     selected_page = st.sidebar.radio(" ", ["Our Mission", "ASL Detection", "Text to ASL", "Resources"])
+
+if __name__ == "__main__":
+    main()
 
