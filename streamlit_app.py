955e11d97c99b841a3c36130121c4fcb88ec7d93
Sandy Guntupalli <72150665+sandy-guntupalli@users.noreply.github.com>
2024-08-26 16:32:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# import cv2
+import cv2
 import streamlit as st
 
 def main():
@@ -7,17 +7,17 @@ def main():
 
     # Create custom navigation buttons for both "About" and "Model"
     selected_page = st.sidebar.radio(" ", ["Our Mission", "ASL Detection", "Text to ASL", "Resources"])
-    # st.title("Webcam Live Feed")
-    # run = st.checkbox('Run')
-    # FRAME_WINDOW = st.image([])
-    # camera = cv2.VideoCapture(0)
+    st.title("Webcam Live Feed")
+    run = st.checkbox('Run')
+    FRAME_WINDOW = st.image([])
+    camera = cv2.VideoCapture(0)
     
-    # while run:
-    #     _, frame = camera.read()
-    #     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    #     FRAME_WINDOW.image(frame)
-    # else:
-    #     st.write('Stopped')
+    while run:
+        _, frame = camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame)
+    else:
+        st.write('Stopped')
     
 if __name__ == "__main__":
     main()
