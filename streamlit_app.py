d26b4c2866501b58361e97460875b63d4cdff1d9
Sandy Guntupalli <72150665+sandy-guntupalli@users.noreply.github.com>
2024-08-26 16:38:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,38 @@
 import cv2
 import streamlit as st
+import time
 
 def main():
     st.sidebar.title("ASL AI :i_love_you_hand_sign:")
     st.sidebar.markdown("---")
 
-    # Create custom navigation buttons for both "About" and "Model"
+    # Create custom navigation buttons
     selected_page = st.sidebar.radio(" ", ["Our Mission", "ASL Detection", "Text to ASL", "Resources"])
     st.title("Webcam Live Feed")
 
     run = st.checkbox('Run')
     FRAME_WINDOW = st.image([])
 
-    # Initialize camera
     camera = cv2.VideoCapture(0)
-    
-    if run:
-        while run:
-            ret, frame = camera.read()
-            if not ret:
-                st.error("Failed to capture image")
-                break
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            FRAME_WINDOW.image(frame)
-    else:
-        st.write('Stopped')
 
-    # Release the camera when done
+    # Check if the camera is opened successfully
+    if not camera.isOpened():
+        st.error("Could not open the camera.")
+        return
+
+    # Small delay to ensure the camera is initialized
+    time.sleep(1)
+
+    while run:
+        ret, frame = camera.read()
+        if not ret:
+            st.error("Failed to capture image")
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame)
+
     camera.release()
+    st.write('Stopped')
 
 if __name__ == "__main__":
     main()
