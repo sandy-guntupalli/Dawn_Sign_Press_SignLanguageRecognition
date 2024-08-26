c331a4cb1a4755a06bc8df6df708b719a32727ee
Sandy Guntupalli <72150665+sandy-guntupalli@users.noreply.github.com>
2024-08-26 19:05:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,11 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import av
 
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
 def main():
     st.sidebar.title("ASL AI :i_love_you_hand_sign:")
     st.sidebar.markdown("---")
@@ -10,18 +15,7 @@ def main():
     selected_page = st.sidebar.radio(" ", ["Our Mission", "ASL Detection", "Text to ASL", "Resources"])
     st.title("Webcam Live Feed")
 
-    flip = st.checkbox("Flip")
-
-
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-
-    flipped = img[::-1,:,:] if flip else img
-
-    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
-
-
-webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+    webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
 
 if __name__ == "__main__":
     main()
