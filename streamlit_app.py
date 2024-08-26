7a08025dc03ecaef70567a3e512134775290751f
Sandy Guntupalli <72150665+sandy-guntupalli@users.noreply.github.com>
2024-08-26 19:19:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,10 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import av
 
-
-flip = st.checkbox("Flip")
-
-
 def video_frame_callback(frame):
     img = frame.to_ndarray(format="bgr24")
 
-    flipped = img[:, ::-1, :] if flip else img
+    mirrored = img[:, ::-1, :]
 
     return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
