d6eeca4f4266ab5459b25bceb5d11e6479780568
Sandy Guntupalli <72150665+sandy-guntupalli@users.noreply.github.com>
2024-08-26 19:06:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ from streamlit_webrtc import webrtc_streamer
 import av
 
 
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
+# def video_frame_callback(frame):
+#     img = frame.to_ndarray(format="bgr24")
+#     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 def main():
     st.sidebar.title("ASL AI :i_love_you_hand_sign:")
@@ -15,7 +15,7 @@ def main():
     selected_page = st.sidebar.radio(" ", ["Our Mission", "ASL Detection", "Text to ASL", "Resources"])
     st.title("Webcam Live Feed")
 
-    webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+    webrtc_streamer(key="example")
 
 if __name__ == "__main__":
     main()
