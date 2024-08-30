ba0190310f076c9b4b23938d645c018a78d760f2
sabrinaboyle <100177473+sabrinaboyle@users.noreply.github.com>
2024-08-30 17:38:30 -0400

    Update 2_Easy_Mode.py

--- pages/2_Easy_Mode.py
+++ pages/2_Easy_Mode.py
@@ -7,3 +7,34 @@ if "page" not in st.session_state:
     st.session_state["page"] = "easypage"
 
 st.session_state["page"] = "easypage"
+st.markdown(page_with_webcam_video(), unsafe_allow_html=True)
+
+if "alphabet" not in st.session_state:
+    st.session_state["alphabet"] = 0
+
+ALPHABET_LIST = {
+    0:"A",
+    1:"B",
+    2:"C",
+    3:"D",
+    4:"E",
+    5:"F",
+    6:"G",
+    7:"H",
+    8:"I",
+    9:"J",
+    10:"K",
+    11:"L",
+    12:"M",
+    13:"N",
+    14:"O",
+    15:"P",
+    16:"R",
+    17:"S",
+    18:"T",
+    19:"U",
+    20:"V",
+    21:"W",
+    22:"X",
+    23:"Y",
+}
