accd983d878699c1e9d28be1e6e6b0a29e182ffa
sabrinaboyle <100177473+sabrinaboyle@users.noreply.github.com>
2024-08-30 13:17:13 -0400

    Update 1_Home.py

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="Home")
+
 # Check if 'page' exists in session state, if not, initialize it
 if "page" not in st.session_state:
     st.session_state["page"] = "homepage"
