4e4da02702251f1d30df87ecffaa103985ce65a0
sabrinaboyle <100177473+sabrinaboyle@users.noreply.github.com>
2024-08-30 18:39:23 -0400

    Update and rename Streamlit_app.py to Home.py

--- Home.py
+++ Home.py
@@ -1,29 +1,13 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
 
 st.set_page_config(
     page_title = "Home"
 )
 
 
-def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-    # Convert frame to a NumPy array
-    img = frame.to_ndarray(format="bgr24")
-    
-    # Flip the video horizontally 
-    img = img[:, ::-1, :]
-
-    # Convert the frame back to av.VideoFrame
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
-
 def main():
 
-    st.title("Webcam Live Feed")
-
-    # Stream webcam feed
-    #webrtc_streamer(key="example", video_frame_callback=video_frame_callback, 
-                    #rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+    st.title("NAME")
 
 if __name__ == "__main__":
     main()
