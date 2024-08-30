d49e7211069ce497960fe6ac394365156c0a2fa1
sabrinaboyle <100177473+sabrinaboyle@users.noreply.github.com>
2024-08-30 13:17:45 -0400

    Update 3_Hard_Mode.py

--- pages/3_Hard_Mode.py
+++ pages/3_Hard_Mode.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.set_page_config(page_title="Hard Mode")
+
+# Check if 'page' exists in session state, if not, initialize it
+if "page" not in st.session_state:
+    st.session_state["page"] = "hardpage"
+
+st.session_state["page"] = "hardpage"
