6b8d844bd2c77f1614fd258fa87f2a62b10ca862
sabrinaboyle <100177473+sabrinaboyle@users.noreply.github.com>
2024-08-30 13:16:24 -0400

    Update 1_Home.py

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,1 +1,7 @@
+import streamlit as st
 
+# Check if 'page' exists in session state, if not, initialize it
+if "page" not in st.session_state:
+    st.session_state["page"] = "homepage"
+
+st.session_state["page"] = "homepage"
