7efdb017446709e2a59403fef7522749b80a2121
Sandy Guntupalli <72150665+sandy-guntupalli@users.noreply.github.com>
2024-08-31 14:15:41 -0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 video_urls = {
-    "A": "https://drive.google.com/file/d/1aSX0NSuFCiyl1DYgmjV76LCaWOcH6FIb/view?usp=sharing",
+    "A": "https://www.lifeprint.com/asl101/fingerspelling/abc-gifs/a.gifg",
     "B": "https://videos-asl.lingvano.com/9101-480p.mp4",
     "C": "https://videos-asl.lingvano.com/9102-480p.mp4",
     "D": "https://videos-asl.lingvano.com/9133-480p.mp4",
