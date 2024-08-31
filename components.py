79721af4c5b108ee99a7db111f12fc0bb54cae2d
Sandy Guntupalli <72150665+sandy-guntupalli@users.noreply.github.com>
2024-08-31 15:14:40 -0400

    Update components.py

--- components.py
+++ components.py
@@ -19,7 +19,7 @@ def update_video(character):
     if st.session_state["page"]=="easypage":
         gif_url = f"https://www.lifeprint.com/asl101/fingerspelling/abc-gifs/{character.lower()}.gif"
         return f"""
-        <div class="video-wrapper">
-        <img src="{gif_url}" width="350" height="290" style="transform: scale(1.75); border-radius: 1rem;">
+       <div style="text-align: center;">
+        <img src="{gif_url}" alt="Sign language gif" style="width: 350px; height: 290px;">
         </div>
         """
