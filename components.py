cb33b2fd5d53a55ab02238a024f9aa22a6743464
sabrinaboyle <100177473+sabrinaboyle@users.noreply.github.com>
2024-09-01 16:26:08 -0400

    Update components.py

--- components.py
+++ components.py
@@ -17,10 +17,10 @@ def progress_bar(prog):
 
 def update_video(character):
     if st.session_state["pages"]=="easypage":
-        gif_url = f"https://www.lifeprint.com/asl101/fingerspelling/abc-gifs/{character.lower()}.gif"
-        st.image(gif_url, width=350, caption=f"Sign language: {character}")
-       #  return f"""
-       # <div style="text-align: center;">
-       #  <img src="{gif_url}" alt="Sign language gif" style="width: 350px; height: 290px;">
-       #  </div>
-       #  """
+        #gif_url = f"https://www.lifeprint.com/asl101/fingerspelling/abc-gifs/{character.lower()}.gif"
+        #st.image(gif_url, width=350, caption=f"Sign language: {character}")
+        return f"""
+        <div style="text-align: center;">
+        <img src="{gif_url}" alt="Sign language gif" style="width: 350px; height: 290px;">
+        </div>
+        """
