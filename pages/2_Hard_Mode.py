d7842c582eb0d3c00fe791f972249ccb2b2617be
Sandy Guntupalli <72150665+sandy-guntupalli@users.noreply.github.com>
2024-09-02 23:57:05 -0400

    Update 2_Hard_Mode.py

--- pages/2_Hard_Mode.py
+++ pages/2_Hard_Mode.py
@@ -9,4 +9,31 @@ if "page" not in st.session_state:
 
 st.session_state["page"] = "hardpage"
 
+def set_font_style():
+    return """
+    <style>
+        /* Apply Century Gothic font to the whole app */
+        * {
+            font-family: 'Century Gothic', sans-serif;
+        }
+        
+        /* Specific styling for the title */
+        .st-bd h1 {
+            font-family: 'Century Gothic', sans-serif;
+        }
+    </style>
+    """
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #89CDD3;  /* Light blue background */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True)
+    
+st.markdown(set_font_style(), unsafe_allow_html=True)
+
 st.markdown(page_setup(), unsafe_allow_html=True)
